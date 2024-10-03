49d22bd453161a20010c12c67b64fff1e2eb40a8
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-02 22:30:52 -0300

    aulas

---
+++ aula05/js/main.js
@@ -0,0 +1,37 @@
+/*
+function caixa(texto, tam) {
+    console.log("---")
+    console.log(texto)
+    console.log("---")
+}
+
+caixa("Ana", 3)
+caixa("Bia", 3)
+caixa("João", 4)
+
+function tracos(qtde) {
+    let texto = ""
+    for(let i = 0; i < qtde; i++) {
+        texto = texto + "-"
+    }
+    console.log(texto)
+}
+
+function caixa(texto, qtde) {
+    tracos(qtde)
+    console.log(texto)
+    tracos(qtde)
+}
+
+*/
+
+function areaRetangulo(larg, alt) {
+    let area = larg * alt
+    return area
+}
+
+let x = 5
+
+let soma = function(x, y) {
+    return x + y
+}
