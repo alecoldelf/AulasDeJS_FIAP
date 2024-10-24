666801df8046eb9405eff12bb5f965f559bc2c6c
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-23 21:57:37 -0300

    Aulas

---
+++ aula07/main.py
@@ -0,0 +1,19 @@
+#Aula substituta pq o professor faltou
+
+import pyautogui
+import time
+
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+
+pyautogui.click(x=250, y=66)
+
+pyautogui.write("https://chatgpt.com/")
+pyautogui.press("enter")
+
+time.sleep(2)
+pyautogui.write("")
+
+pyautogui.write("AAAAAAAAAAAAAAA")
+pyautogui.press("enter")
